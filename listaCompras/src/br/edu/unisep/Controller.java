7ef0ff04bf4acfe5664575f31346c62756f243e0
rcpadilha <robertocesardasilva@gmail.com>
2019-03-09 00:49:03 -0300

    Aula 08/03

---
+++ listaCompras/src/br/edu/unisep/Controller.java
@@ -0,0 +1,79 @@
+package br.edu.unisep;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Paint;
+import javafx.util.Callback;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML private TextField txtDescricao;
+
+    @FXML private TextField txtMercadoA;
+    @FXML private TextField txtMercadoB;
+    @FXML private TextField txtMercadoC;
+
+    @FXML private TableView<ProdutoVO> tabProdutos;
+    @FXML private TableColumn<ProdutoVO, String> colDescricao;
+    @FXML private TableColumn<ProdutoVO, Double> colMercadoA;
+    @FXML private TableColumn<ProdutoVO, Double> colMercadoB;
+    @FXML private TableColumn<ProdutoVO, Double> colMercadoC;
+
+    private ObservableList<ProdutoVO> produtos;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        produtos = FXCollections.observableArrayList();
+        tabProdutos.setItems(produtos);
+
+        colDescricao.setCellValueFactory(new PropertyValueFactory<>("descricao"));
+        colMercadoA.setCellValueFactory(new PropertyValueFactory<>("mercadoA"));
+        colMercadoB.setCellValueFactory(new PropertyValueFactory<>("mercadoB"));
+        colMercadoC.setCellValueFactory(new PropertyValueFactory<>("mercadoC"));
+
+    }
+
+    public void adicionar(ActionEvent event) {
+        var p = new ProdutoVO();
+        p.setDescricao(txtDescricao.getText());
+        p.setMercadoA(Double.valueOf(txtMercadoA.getText()));
+        p.setMercadoB(Double.valueOf(txtMercadoB.getText()));
+        p.setMercadoC(Double.valueOf(txtMercadoC.getText()));
+
+        produtos.add(p);
+
+        limpar();
+    }
+
+    public void excluir(ActionEvent event) {
+        var p = tabProdutos.getSelectionModel().getSelectedItem();
+
+        if (p != null)  {
+            produtos.remove(p);
+        } else {
+            var msg = new Alert(Alert.AlertType.WARNING, "Selecione um produto para excluir");
+            msg.show();
+        }
+    }
+
+    private void limpar() {
+        txtDescricao.clear();
+        txtMercadoA.clear();
+        txtMercadoB.clear();
+        txtMercadoC.clear();
+    }
+
+}
---
+++ listaCompras/src/br/edu/unisep/ProdutoVO.java
@@ -0,0 +1,44 @@
+package br.edu.unisep;
+
+public class ProdutoVO {
+
+    private String descricao;
+
+    private Double mercadoA;
+
+    private Double mercadoB;
+
+    private Double mercadoC;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getMercadoA() {
+        return mercadoA;
+    }
+
+    public void setMercadoA(Double mercadoA) {
+        this.mercadoA = mercadoA;
+    }
+
+    public Double getMercadoB() {
+        return mercadoB;
+    }
+
+    public void setMercadoB(Double mercadoB) {
+        this.mercadoB = mercadoB;
+    }
+
+    public Double getMercadoC() {
+        return mercadoC;
+    }
+
+    public void setMercadoC(Double mercadoC) {
+        this.mercadoC = mercadoC;
+    }
+}
